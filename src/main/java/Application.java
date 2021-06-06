a2e12d6db8c23be3940fa48bf458d2b0276c26d4
Amila1610 <amilahasovic@hotmail.com>
2021-06-06 17:12:20 +0200

    Java + Maven/ Hibernate + MySQL/ Session Factory/ Hibernate mapping

---
+++ src/main/java/Application.java
@@ -0,0 +1,21 @@
+import entity.Product;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
+
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory= HibernateUtil.getSessionFactory();
+        try(Session session=sessionFactory.openSession()){
+            Query<Product> query=session.createQuery("from Product");
+            List<Product> productList=query.list();
+            productList.forEach(System.out::println);
+        }catch (HibernateException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
